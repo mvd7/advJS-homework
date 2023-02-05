2aa4366eea9881ccc566a34d63681227936fc6c0
Aleksandar Dimov <dimov0703@gmail.com>
2023-02-05 15:05:51 +0100

    fixed fetch

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -3,19 +3,14 @@ console.log(`hi`);
 const API_URL =
   "https://api.openweathermap.org/data/3.0/onecall?lat=41.99646&lon=21.43141&units=metric&exclude=minutely&appid=83cf676a48739fd57b023a3d32f2ef8b";
 
-fetch(API_URL)
-  .then((res) => res.json())
-  .then((data) => {
-    const homeBtn = document.querySelector(`.home-btn`);
-    const aboutBtn = document.querySelector(`.about-btn`);
-    const hourlyBtn = document.querySelector(`.hourly-btn`);
-    const mainEl = document.querySelector(`main`);
+const homeBtn = document.querySelector(`.home-btn`);
+const aboutBtn = document.querySelector(`.about-btn`);
+const hourlyBtn = document.querySelector(`.hourly-btn`);
+const mainEl = document.querySelector(`main`);
 
-    const renderHomePage = (mainEl, data) => {
-      const currentDate = new Date(data.current.dt * 1000);
-
-      mainEl.innerHTML = "";
-      mainEl.innerHTML = `<div class="current-container">
+const createCardHomePage = (data) => {
+  mainEl.innerHTML = "";
+  mainEl.innerHTML = `<div class="current-container">
   <div class="left-side">
     <div class="location">
       <h3>${data.timezone}</h3>
@@ -35,7 +30,7 @@ fetch(API_URL)
     <h3>Weather Next 7 Days</h3>
     <div class="right-side-content">
       <div class="day-card-container">
-
+ 
       </div>
       <div class="line"></div>
       <h5 class="min-temp">Feels Like: ${data.current.feels_like}</h5>
@@ -45,11 +40,14 @@ fetch(API_URL)
   </div>
 </div>
 `;
-      let dailyHTML = "";
-      for (let el of data.daily) {
-        const dailyDate = new Date(el.dt * 1000);
+};
 
-        dailyHTML += `
+const createCardDailyData = (data) => {
+  let dailyHTML = "";
+  for (let el of data.daily) {
+    const dailyDate = new Date(el.dt * 1000);
+
+    dailyHTML += `
     <div class="day-card">
     <p>${dailyDate.toLocaleDateString("fr")}</p>
     <img src="http://openweathermap.org/img/wn/${
@@ -61,30 +59,35 @@ fetch(API_URL)
     <p class="max-temp">Min:${el.temp.max}°C</p>
     <p class="humidity">Humidity: ${el.humidity}%</p>
   </div> `;
-      }
+  }
 
-      document.querySelector(".day-card-container").innerHTML = dailyHTML;
-    };
+  document.querySelector(".day-card-container").innerHTML = dailyHTML;
+};
 
-    const renderAboutPage = (mainEl) => {
-      mainEl.innerHTML = "";
-      mainEl.innerHTML = `      <div class="about-box">
+const renderHomePage = (data) => {
+  createCardHomePage(data);
+  createCardDailyData(data);
+};
+
+const renderAboutPage = () => {
+  mainEl.innerHTML = "";
+  mainEl.innerHTML = `      <div class="about-box">
   <div class="about-container">
     <h1>Aleksandar Dimov</h1>
     <img src="./assets/person.png" alt="person image" />
     <p>Hello, my name is Aleksandar im 19 years old and im student at the SEDC Acedemy for programming and this is my Weather App.</p>
     <a href="https://github.com/mvd7" target="_blank">Click here for my GitHub repository</a>
 </div>`;
-    };
+};
 
-    const renderHourlyPage = (mainEl, data) => {
-      mainEl.innerHTML = `<div class="hourly-box"><h1>Hourly Weather</h1>
+const renderHourlyPage = (data) => {
+  mainEl.innerHTML = `<div class="hourly-box"><h1>Hourly Weather</h1>
   <div class="hourly-container"></div></div>
   `;
-      let hourlyHTML = ``;
-      for (let el of data.hourly) {
-        const dailyDate = new Date(el.dt * 1000);
-        hourlyHTML += `
+  let hourlyHTML = ``;
+  for (let el of data.hourly) {
+    const dailyDate = new Date(el.dt * 1000);
+    hourlyHTML += `
     <div class="day-card">
           <p class="clock">${dailyDate.toLocaleTimeString("fr")}</p>
           <img
@@ -98,20 +101,27 @@ fetch(API_URL)
           <p>${el.weather[0].description}</p>
         </div>
     `;
-      }
-      document.querySelector(`.hourly-container`).innerHTML = hourlyHTML;
-    };
+  }
+  document.querySelector(`.hourly-container`).innerHTML = hourlyHTML;
+};
 
-    hourlyBtn.addEventListener("click", () => {
-      renderHourlyPage(mainEl, data);
-    });
+function fetchApiWeather() {
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      hourlyBtn.addEventListener("click", () => {
+        renderHourlyPage(data);
+      });
 
-    homeBtn.addEventListener("click", () => {
-      renderHomePage(mainEl, data);
-    });
+      homeBtn.addEventListener("click", () => {
+        renderHomePage(data);
+      });
 
-    aboutBtn.addEventListener("click", () => {
-      renderAboutPage(mainEl);
+      aboutBtn.addEventListener("click", () => {
+        renderAboutPage();
+      });
+      window.onload(renderHomePage(data));
     });
-    window.onload(renderHomePage(mainEl, data));
-  });
+}
+
+fetchApiWeather();
